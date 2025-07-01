a9a7feeaa1f6b82dd85e14467a05e80c3be55fd1
EMRE <EMRE@DESKTOP-OO8LL34>
2025-07-01 23:46:26 +0300

    Veritabanı baglantısı sağlanarak ile migration alındı.

--- Digital_API_Layer/Program.cs
+++ Digital_API_Layer/Program.cs
@@ -1,9 +1,12 @@
+using Digital_Core_Layer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCoreRegisterServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ Digital_Core_Layer/ServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Digital_Domain_Layer.Entities;
+using Digital_Persistence_Layer.AppDbContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Digital_Core_Layer
+{
+    public static class ServiceRegistration
+    {
+        public static void AddCoreRegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentityCore<User>().AddEntityFrameworkStores<ApplicationDbContext>();
+        }
+    }
+}
