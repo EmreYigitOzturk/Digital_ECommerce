0e6b96c3d1781bb2b835dc780e95127d52a2d9f2
EMRE <EMRE@DESKTOP-OO8LL34>
2025-07-02 23:39:41 +0300

    Repository classes added.

---
+++ Digital_Persistence_Layer/Repositories/Abstract/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Digital_Domain_Layer.Extensions;
+
+namespace Digital_Persistence_Layer.Repositories.Interface
+{
+    public interface IRepository<T> where T : new()
+    {
+
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(Guid Id,T entity);
+        Task DeleteAsync(Guid Id);
+        Task<T> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+
+        public Task<PagedResult<T>> GetPagedResult(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, int pageNumber = 1, int pageSize = 10, params Expression<Func<T, object>>[] includeProperties);
+
+    }
+}
---
+++ Digital_Persistence_Layer/Repositories/Concrete/Repository.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Digital_Domain_Layer.Extensions;
+using Digital_Persistence_Layer.AppDbContext;
+using Digital_Persistence_Layer.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace Digital_Persistence_Layer.Repositories.Concrete
+{
+    public class Repository<T> : IRepository<T> where T : class, new()
+    {
+        private readonly ApplicationDbContext context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        private DbSet<T> Table { get => context.Set<T>(); }
+
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            await Table.AddAsync(entity);
+            if (await context.SaveChangesAsync() > 0)
+            {
+                return entity;
+            }
+            return null;
+        }
+
+        public async Task DeleteAsync(Guid Id)
+        {
+            var entity = await Table.FindAsync(Id);
+            if (entity is not null)
+            {
+                Table.Remove(entity);
+                await context.SaveChangesAsync();
+            }
+
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            IEnumerable<T> entities = await Table.ToListAsync();
+            if (entities is not null)
+            {
+                return entities;
+            }
+            return null;
+
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            var entity = await Table.FindAsync(id);
+            if (entity is not null)
+            {
+                return entity;
+            }
+            return null;
+        }
+
+        public async Task<T> UpdateAsync(Guid Id, T entity)
+        {
+            var entityToUpdate = await Table.FindAsync(Id);
+            if (entityToUpdate is not null)
+            {
+                context.Entry(entityToUpdate).CurrentValues.SetValues(entity);
+                if (await context.SaveChangesAsync() > 0)
+                {
+                    return entityToUpdate;
+                }
+            }
+            return null;
+        }
+
+        public async Task<PagedResult<T>> GetPagedResult(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, int pageNumber = 1, int pageSize = 10, params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = Table.AsQueryable();
+            if (filter is not null)
+            {
+                query = query.Where(filter);
+            }
+            int totalCount = await query.CountAsync();
+            if (orderBy is not null)
+            {
+                query = orderBy(query);
+            }
+            if (includeProperties.Length > 0)
+            {
+                foreach (var item in includeProperties)
+                {
+                    query = query.Include(item);
+                }
+            }
+            List<T> items = await query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PagedResult<T>
+            {
+                Items = items,
+                TotalCount = totalCount,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+
+        }
+
+
+    }
+}
